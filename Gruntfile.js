2d13d7d7098e8182ab863237e90c22a7f9c7e2b3
Dayna <daynajblackwell@gmail.com>
2018-10-13 22:29:18 -0700

    Continuing to scaffold app

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+    
+    grunt.initConfig({
+        // concat: {
+        //     {}
+        // }
+    });
+
+    // Load plugins here..
+    
+    // grunt.loadNpmTasks('grunt-contrib-concat');
+    
+    
+    // grunt.loadNpmTasks('')
+
+    // Register tasks
+    
+    
+        
+    
+    
+    // grunt.registerTask('run', () => {
+    //     console.log('I am running');
+    // });
+
+    // grunt.registerTask('sleep', () => {
+    //     console.log('I am sleeping');
+    // });
+
+    // grunt.registerTask('all',['run','sleep']);
+
+}
